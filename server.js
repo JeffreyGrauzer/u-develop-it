b85ff888c94949b750b12b2524424219ba97dcfb
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-01-26 21:48:26 -0600

    installed mysql2

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require("mysql2");
 const express = require("express");
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -5,7 +6,22 @@ const app = express();
 //Express middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+// Connect to database
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    // Your MySql username,
+    user: "root",
+    // Your MySql password
+    password: "schmotta",
+    database: "election",
+  },
+  console.log("Connected to the election database")
+);
 
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+  console.log(rows);
+});
 // Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
