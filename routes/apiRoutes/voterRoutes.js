7ed8eeba1113540eaa2592a8b390c82d806058ef
JeffreyGrauzer <93175521+JeffreyGrauzer@users.noreply.github.com>
2022-01-29 18:24:55 -0600

    added 12.5 code

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -1,9 +1,10 @@
-const express = require("express");
+const express = require('express');
 const router = express.Router();
-const db = require("../../db/connection");
-const inputCheck = require("../../utils/inputCheck");
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
 
-router.get("/voters", (req, res) => {
+// Get all voters alphabetized by last name
+router.get('/voters', (req, res) => {
   const sql = `SELECT * FROM voters ORDER BY last_name`;
 
   db.query(sql, (err, rows) => {
@@ -12,13 +13,14 @@ router.get("/voters", (req, res) => {
       return;
     }
     res.json({
-      message: "success",
-      data: rows,
+      message: 'success',
+      data: rows
     });
   });
 });
+
 // Get single voter
-router.get("/voter/:id", (req, res) => {
+router.get('/voter/:id', (req, res) => {
   const sql = `SELECT * FROM voters WHERE id = ?`;
   const params = [req.params.id];
 
@@ -28,20 +30,22 @@ router.get("/voter/:id", (req, res) => {
       return;
     }
     res.json({
-      message: "success",
-      data: row,
+      message: 'success',
+      data: row
     });
   });
 });
 
-router.post("/voter", ({ body }, res) => {
-  // data validation
-  const errors = inputCheck(body, "first_name", "last_name", "email");
+// Create a voter
+router.post('/voter', ({ body }, res) => {
+  // Data validation
+  const errors = inputCheck(body, 'first_name', 'last_name', 'email');
   if (errors) {
     res.status(400).json({ error: errors });
     return;
   }
-  const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?, ?, ?)`;
+
+  const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
   const params = [body.first_name, body.last_name, body.email];
 
   db.query(sql, params, (err, result) => {
@@ -50,12 +54,13 @@ router.post("/voter", ({ body }, res) => {
       return;
     }
     res.json({
-      message: "success",
-      data: body,
+      message: 'success',
+      data: body
     });
   });
 });
 
+// Update a voter's email
 router.put('/voter/:id', (req, res) => {
   // Data validation
   const errors = inputCheck(req.body, 'email');
@@ -84,6 +89,7 @@ router.put('/voter/:id', (req, res) => {
   });
 });
 
+// Delete a voter
 router.delete('/voter/:id', (req, res) => {
   const sql = `DELETE FROM voters WHERE id = ?`;
 
--- server.js
+++ server.js
@@ -1,5 +1,5 @@
-const db = require('./db/connection');
 const express = require('express');
+const db = require('./db/connection');
 const apiRoutes = require('./routes/apiRoutes');
 
 const PORT = process.env.PORT || 3001;
@@ -9,15 +9,13 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Use apiRoutes
 app.use('/api', apiRoutes);
 
-
-
-// Not Found response for unmatched routes
+// Default response for any other request (Not Found)
 app.use((req, res) => {
   res.status(404).end();
 });
-
 
 // Start server after DB connection
 db.connect(err => {
